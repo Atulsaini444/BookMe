efdd068aaeaca38a19e24781d6127cdf2e15fa01
Atul Kumar <atulsaini649@gmail.com>
2024-04-03 08:10:07 +0530

    First commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClinetOnly'
+import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import RegisterModal from './components/modals/RegisterModal'
+import ModalsProvider from './providers/ModalsProvider'
+import getCurrentUser from './actions/getCurrentUser'
+
+const font = Nunito({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'BookMe',
+  description: 'BookMe',
+}
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const currentUser = await getCurrentUser();
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+        <ToasterProvider />
+        <ModalsProvider/>
+          <Navbar  currentUser={currentUser} />
+        </ClientOnly>
+      </body>
+    </html>
+  )
+}
