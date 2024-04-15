329971d0d8f0215a3d24edb6ddec46413f746c8c
Atul Kumar <atulsaini649@gmail.com>
2024-04-15 21:51:17 +0530

    logo updated

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -113,7 +113,7 @@ const Search = () => {
           <div 
             className="
               p-2 
-              bg-rose-500 
+              bg-teal-500 
               rounded-full 
               text-white
             "
